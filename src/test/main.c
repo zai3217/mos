740b3af5c4b4b70cc0b53fa28a83732955596b27
zai <zai3217.goo@gmail.com>
2024-09-18 14:41:47 +0800

    n4

--- src/test/main.c
+++ src/test/main.c
@@ -1,30 +1,6 @@
 #include <stdio.h>
-void int_to_str(int num, char *str){
-    int isNegative = 0;
-    if (num < 0){
-        isNegative = 1; // 标记为负数
-        num = -num;     // 取绝对值
-    }
-    int i = 0;
-    do{
-        str[i++] = (num % 10) + '0'; // 提取最后一位并转换为字符
-        num /= 10;                   // 去掉最后一位
-    } while (num > 0);
-
-    if (isNegative){
-        str[i++] = '-'; // 如果是负数，添加负号
-    };
-
-    // 反转字符串
-    for (int j = 0; j < i / 2; j++){
-        char temp = str[j];
-        str[j] = str[i - j - 1];
-        str[i - j - 1] = temp;
-    }
-}
+#include <mos/types.h>
 int main(){
-    int n = -2123221231;
-    char str[11];
-    int_to_str(n, str);
-    printf("%s\n", str);
+    u16 i = 10;
+    printf("%d",(i-20));
 }
